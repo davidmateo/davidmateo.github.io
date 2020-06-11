49d8dd8508867c80d48592e0318fa8a6e13ddcef
Mateo082003 <63216360+Mateo082003@users.noreply.github.com>
2020-06-11 10:39:29 -0500

    davidmateo.github.io
    calcular el area del rombo,del poligono regular del trapecio, del circulo y el volumen del cubo

--- calculos.js
+++ calculos.js
@@ -1,3 +1,7 @@
+let toInt = (numero) => {
+    return parseInt(numero, 10);
+}
+
 let calcularAreaCuadrado = () => {
     const lado = document.getElementById('lado');
     const resultado = document.getElementById('resultadoAreaCuadrado')
@@ -43,4 +47,74 @@ let calcularArearectangulo = () => {
     const ancho = document.getElementById('ancho_rectangulo')
     const resultadoArearectangulo = document.getElementById('resultadoArearectangulo')
     resultadoArearectangulo.value = (lado.value * ancho.value)
-}  
+}
+let borrarareatrapecio = () => {
+    const base_menor = document.getElementById('base_menor')
+    const base_mayor = document.getElementById('base_mayor')
+    const altura_trapecio = document.getElementById('altura_trapecio')
+    const resultadoareatrapecio = document.getElementById('resultadoareatrapecio')
+    resultadoareatrapecio.value = 0;
+    base_mayor.value = 0;
+    base_menor.value = 0;
+    altura_trapecio.value = 0;
+}
+let calcularareatrapecio = () => {
+    const base_menor = document.getElementById('base_menor')
+    const base_mayor = document.getElementById('base_mayor')
+    const altura_trapecio = document.getElementById('altura_trapecio')
+    const resultadoareatrapecio = document.getElementById('resultadoareatrapecio')
+    resultadoareatrapecio.value = toInt(altura_trapecio.value) * ((toInt(base_mayor.value) + toInt(base_menor.value)) / 2)
+}
+let borrarareacirculo = () => {
+    const radio_circulo = document.getElementById('radio_circulo')
+    const resultadoareacirculo = document.getElementById('resultadoareacirculo')
+    resultadoareacirculo.value = 0;
+    radio_circulo.value = 0;
+}
+let calcularareacirculo = () => {
+    const radio_circulo = document.getElementById('radio_circulo')
+    const resultadoareacirculo = document.getElementById('resultadoareacirculo')
+    resultadoareacirculo.value = (3.14 * (radio_circulo.value * radio_circulo.value))
+}
+let borrarareapoligono = () => {
+    const l_lado = document.getElementById('l_lados')
+    const apotema_p = document.getElementById('apotema_p')
+    const n_lados = document.getElementById('n_lados')
+    const resultadoareapoligono = document.getElementById('resultadoareapoligono')
+    resultadoareapoligono.value = 0;
+    l_lado.value = 0;
+    apotema_p.value = 0;
+    n_lados.value = 0;
+}
+let calcularareapoligono = () => {
+    const l_lado = document.getElementById('l_lados')
+    const apotema_p = document.getElementById('apotema_p')
+    const n_lados = document.getElementById('n_lados')
+    const resultadoareapoligono = document.getElementById('resultadoareapoligono')
+    resultadoareapoligono.value = (n_lados.value*l_lado.value)*(apotema_p.value)/2
+}
+let borrararearombo = () => {
+    const diagonal1 = document.getElementById('diagonal1')
+    const diagonal2 = document.getElementById('diagonal2')
+    const resultadoarearombo = document.getElementById('resultadoarearombo')
+    resultadoarearombo.value = 0;
+    diagonal1.value = 0;
+    diagonal2.value = 0;
+}
+let calculararearombo = () => {
+    const diagonal1 = document.getElementById('diagonal1')
+    const diagonal2 = document.getElementById('diagonal2')
+    const resultadoarearombo = document.getElementById('resultadoarearombo')
+    resultadoarearombo.value = ((diagonal1.value * diagonal2.value) / 2)
+}
+let borrarvolcubo = () => {
+    const l_cubo =document.getElementById('l_cubo')
+    const resultadovolcubo =document.getElementById('resultadovolcubo')
+    l_cubo.value = 0;
+    resultadovolcubo.value = 0;
+}
+let calcularvolcubo = () => {
+    const l_cubo =document.getElementById('l_cubo')
+    const resultadovolcubo =document.getElementById('resultadovolcubo')
+   resultadovolcubo.value = (l_cubo.value*l_cubo.value*l_cubo.value)
+}
